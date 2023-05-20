369f1c1054b07bc811a52e6d958f32dc8eb79c3a
shinshin86 <beagles1986@gmail.com>
2023-05-20 15:34:20 +0900

    Refactor imagesToPdf function to use async/await

--- imagesToPdf.js
+++ imagesToPdf.js
@@ -1,33 +1,42 @@
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 
 const validExt = [".jpg", ".jpeg", ".png"];
 
-function imagesToPdf(directoryPath, outputPath) {
+async function imagesToPdf(directoryPath, outputPath) {
     const doc = new PDFDocument();
+    const output = fs.createWriteStream(outputPath);
+    doc.pipe(output);
 
-    doc.pipe(fs.createWriteStream(outputPath));
+    let files;
 
-    fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
+    try {
+        files = await fsPromises.readdir(directoryPath);
+    } catch(err) {
+        console.log('Unable to scan directory: ' + err);
+        return;
+    }
 
-        let pageCount = 0;
-        files.forEach((file) => {
-            if(validExt.includes(path.extname(file))){
-                if(pageCount !== 0){
-                    doc.addPage();
-                }
-                doc.image(path.join(directoryPath, file), 0, 0, {fit: [595.28, 841.89]});
-
-                pageCount++;
+    let pageCount = 0;
+    for(const file of files) {
+        if(validExt.includes(path.extname(file))){
+            if(pageCount !== 0){
+                doc.addPage();
             }
-        });
+            doc.image(path.join(directoryPath, file), 0, 0, {fit: [595.28, 841.89]});
 
-        doc.end();
-    });
+            pageCount++;
+        }
+    }
+
+    doc.end();
+
+    return new Promise((resolve, reject) => {
+        output.on('finish', resolve);
+        output.on('error', reject);
+    })
 }
 
 module.exports = imagesToPdf;
